0e9eb21e75a3066d9bb58d2dfe2587050b8d932a
shalin51 <shalinpatel51@gmail.com>
2017-02-01 12:56:52 -0600

    Lab 2

---
+++ LAB 2/Code/js/App.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {  
+    $("#btnLogin").on("click", function () {
+        var username = $("#txtUser").val();
+        var password = $("#txtPass").val();
+        if (username === "") {
+            alert("enter usrname");
+        }
+        if (password==="") {
+            alert("enter password");
+        }
+        if (username === $.cookie("username") && password==$.cookie("pass")) {
+            window.open("Home.html");
+        } 
+    });
+    $("#btnRegister").on("click", function () {
+        var username = $("#txtUser").val();
+        var password = $("#txtPass").val();
+        var confirPassword = $("#txtConfirm").val();
+        if (username === "") {
+            alert("enter usrname");
+        }
+        if (password === "") {
+            alert("enter password");
+        }
+        if (password === confirPassword) {
+            $.cookie("username", username);
+            $.cookie("pass", password);
+            window.open("Index.html");
+        }
+        else {
+            alert("Password and Confirm password must be same");
+        }
+    }); 
+});
+
+
+
