09584fb7df2082cbbc053b86c5aa65521ffe9b02
shalin51 <shalinpatel51@gmail.com>
2017-03-16 00:18:21 -0500

    lab 8

---
+++ LAB 8/Code/www/js/app.js
@@ -0,0 +1,29 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var App=angular.module('App', ['ionic','ngCordova']);
+
+ App.run(function($ionicPlatform) {
+
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+App.controller('HomeCtrl',function($scope,$cordovaToast){
+
+  $scope.showToast= function(message, duration, location) {
+        $cordovaToast.show(message, duration, location).then(function(success) {
+            console.log("The toast was shown");
+        }, function (error) {
+            console.log("The toast was not shown due to " + error);
+        });
+  }
+});
